2f01b3ded606e4012c926116fe3e1530df4ee500
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-12-10 17:48:45 +0200

    Added content to footer and styled it as well

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  date: Date = new Date();
+
   constructor() {}
 
   ngOnInit(): void {}

44472d591c2af6016bee0a62569213d3edd084bb
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-12-16 12:37:36 +0200

    Added movie data transrafre object and updated the service

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { MovieDto } from '../models/movie';
 
 @Injectable({
   providedIn: 'root'
@@ -7,9 +8,10 @@ import { HttpClient } from '@angular/common/http';
 export class MoviesService {
   baseUrl = 'https://api.themoviedb.org/3';
   apiKey = '8c247ea0b4b56ed2ff7d41c9a833aa77';
+
   constructor(private http: HttpClient) {}
 
   getMovies(type: string = 'upcoming') {
-    return this.http.get(`${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`);
+    return this.http.get<MovieDto>(`${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`);
   }
 }

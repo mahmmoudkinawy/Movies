dda48ba3d6532822c8063efccdb60c21e2e392ee
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-12-18 14:18:43 +0200

    Get videos from the API

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MoviesService } from '../../services/movies.service';
-import { Movie } from '../../models/movie';
-import { IMAGES_SIZES } from 'src/app/constants/images-sizes';
+import { Movie, MovieVideo } from '../../models/movie';
+import { IMAGES_SIZES } from '../../constants/images-sizes';
 
 @Component({
   selector: 'app-movie',
@@ -11,18 +11,26 @@ import { IMAGES_SIZES } from 'src/app/constants/images-sizes';
 })
 export class MovieComponent implements OnInit {
   movie: Movie | null = null;
+  movieVideos: MovieVideo[] = [];
   imagesSizes = IMAGES_SIZES;
   constructor(private route: ActivatedRoute, private moviesSerivce: MoviesService) {}
 
   ngOnInit(): void {
     this.route.params.subscribe(({ id }) => {
       this.getMovie(id);
+      this.getMovieVideos(id);
     });
   }
 
   getMovie(id: string) {
     this.moviesSerivce.getMovie(id).subscribe((movieData) => {
       this.movie = movieData;
+    });
+  }
+
+  getMovieVideos(id: string) {
+    this.moviesSerivce.getMovieVideos(id).subscribe((movieVideosData) => {
+      this.movieVideos = movieVideosData;
     });
   }
 }

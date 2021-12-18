51f2d8154898c0c402633329ca7407830bce5f48
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-12-18 19:27:14 +0200

    Added OnDestroy to help us avoid memory leak.

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,24 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MoviesService } from '../../services/movies.service';
 import { Movie, MovieCredits, MovieImages, MovieVideo } from '../../models/movie';
 import { IMAGES_SIZES } from '../../constants/images-sizes';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.scss']
 })
-export class MovieComponent implements OnInit {
+export class MovieComponent implements OnInit, OnDestroy {
   movie: Movie | null = null;
   movieVideos: MovieVideo[] = [];
   movieImages: MovieImages | null = null;
   movieCredits: MovieCredits | null = null;
   imagesSizes = IMAGES_SIZES;
+
   constructor(private route: ActivatedRoute, private moviesSerivce: MoviesService) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(({ id }) => {
+    this.route.params.pipe(first()).subscribe(({ id }) => {
       this.getMovie(id);
       this.getMovieVideos(id);
       this.getMovieImages(id);
@@ -48,5 +50,9 @@ export class MovieComponent implements OnInit {
     this.moviesSerivce.getMovieCredits(id).subscribe((movieCreditsData) => {
       this.movieCredits = movieCreditsData;
     });
+  }
+
+  ngOnDestroy(): void {
+    console.log('component destroyed yala');
   }
 }

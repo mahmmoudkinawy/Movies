e98ed4cce87f83880d987a5e412fc33b1748bfe7
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-12-18 12:54:54 +0200

    Get data from the API

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MoviesService } from '../../services/movies.service';
+import { Movie } from '../../models/movie';
 
 @Component({
   selector: 'app-movie',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie.component.scss']
 })
 export class MovieComponent implements OnInit {
+  movie: Movie | null = null;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private moviesSerivce: MoviesService) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe(({ id }) => {
+      this.getMovie(id);
+    });
   }
 
+  getMovie(id: string) {
+    this.moviesSerivce.getMovie(id).subscribe((movieData) => {
+      this.movie = movieData;
+    });
+  }
 }

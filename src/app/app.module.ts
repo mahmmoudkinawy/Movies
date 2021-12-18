32dbe4f47ce02583cd31190bf476f21b4a6e2faf
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-12-18 12:43:34 +0200

    Added route to indvidual item details.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ItemsBannerComponent } from './components/items-banner/items-banner.com
 import { ItemComponent } from './components/item/item.component';
 
 import { PaginatorModule } from 'primeng/paginator';
+import { MovieComponent } from './pages/movie/movie.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { PaginatorModule } from 'primeng/paginator';
     MoviesComponent,
     SliderComponent,
     ItemsBannerComponent,
-    ItemComponent
+    ItemComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
